d05854d16ad71b103157cd991a356fdaa3343fd6
NullAux <113611773+NullAux@users.noreply.github.com>
2023-12-14 16:00:43 +0000

    ADDER
    Function for TM Adder. (and beginning of function to encode TMs)

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,6 @@
 // This is done for the purpose of one individual's practice with these models.
 // See code comments for page citations. 
 
-//Evaluate postfix expression - p 424
 
 #include <iostream>
 #include <string>
@@ -161,16 +160,131 @@ std::string ConvertToPrefixNotation(std::string inputString)
 	}
 }
 
+//Turing Machines
+//Cohen 1997:456,456 - A turning machine consists of Alphabet of input letters, tape of cells incl blanks,
+//a tape head beginning at first cell, alphabet of characters on the tape, finite num of states (one start, one+ halt)
+//and a program, which dictates from a state and read letter > change state, print to tape, and move tape head (Letter, letter, direction)
+//What follows are a number of TMs, taken from examples by Cohen
+//For all, the input alphabet will be a and b (used to write unary numbers) and the tape alphabet will be a, b, and blank (ie erase, using a c++ method)
+//Each TM will be written as a number of 'rows', each representing the program information (see Cohen 1997:548 on encoding TMs)
+//State 1 is start and State 2 is halt
+//The result is a string of a's and b's, representing a number in unary
+
+struct TMRow
+{
+	int from;
+	int to;
+	char read;
+	char write;
+	char move;
+
+	TMRow(int iFrom, int iTo, char iRead, char iWrite, char iMove) : 
+		from(iFrom), to(iTo), read(iRead), write(iWrite), move(iMove) {}
+
+	TMRow(int iTo) : to(iTo) {};
+
+};
+
+//Addition. Accepts a string in form a*ba*, meaning some numbers of a's (representing two unary numbers) seperated by one b
+//See Cohen 1997:595
+std::string ADDER(std::string input)
+{
+	//Input alphabet is a and b. Tape alphabet is a, b, and blank (erase, represented by #). The tape is the input string.
+	int tapehead = 0;//tapehead begins at 'first cell'. Since the tape here is an std::string, which begins indexing at 0, tapehead begins at 0
+	int state = 1;
+	input.append("#", 1);//Adding a blank to the tape end
+
+	//Program (in full)
+	struct programInstructions {
+		TMRow One = TMRow(1, 1, 'a', 'a', 'R');
+		TMRow Two = TMRow(1, 2, 'b', 'a', 'R');
+		TMRow Three = TMRow(2, 2, 'a', 'a', 'R');
+		TMRow Four = TMRow(2, 3, '#', '#', 'L');
+		TMRow Five = TMRow(3, 4, 'a', '#', 'R');
+		//State 2 should be the halt state, but here it's state 4 (the state after executing instruction Five). This
+		//function will manually check this, but it's something to fix for a generalised TM in the future.
+	};
+	programInstructions programInstructions;
+
+	//Program (in a std::vector, for convinient access)
+	std::vector<TMRow> program =
+	{
+		programInstructions.One,
+		programInstructions.Two,
+		programInstructions.Three,
+		programInstructions.Four,
+		programInstructions.Five,
+	};
+
+	//Executing the program
+	bool haltProgram = false;
+	while (!haltProgram)
+	{
+		//Find the next instruction
+		TMRow nextInstruction(0);//Initializes 'nextInstruction' with 'to' set to 0 ie an invalid state.
+		//This will be overwritten if a valid instruction from the state+read is found. If it isn't,
+		//then it means the TM would crash at this point, and likewise this method should terminate.
+		for (TMRow instruction : program)
+		{
+			//If correct state and and correct read:
+			if (instruction.from == state && input[tapehead] == instruction.read)//check 'input[tapehead]' plays nice
+			{
+				nextInstruction = instruction;
+			}
+		}
+		//If no valid instruction is found, the program will terminate here.
+		//This is analogous to a TM crashing when reading an input which corresponds to no edge from the current state.
+		if (nextInstruction.to == 0)
+		{
+			std::cout << "Bad string input - TM has crashed" << std::endl;
+			return input;//Return the working string to see where it crashed
+		}
+
+		//Write to tape, change state, move tapehead
+		input[tapehead] = nextInstruction.write;
+		state = nextInstruction.to;
+		switch (nextInstruction.move)
+		{
+		case 'R':
+			tapehead += 1;
+			break;
+		case 'L':
+			tapehead -= 1;
+			//In C++, this ^ can cause an out of bounds error. This is analogous to a TM crashing if the tapehead goes over the left edge, ie bad input.
+			//C++ will also cause an out of bounds error if it goes over the 'right' end. This wouldn't happen on a TM, since it has an infinite series of blanks
+			//at the end. It *shouldn't* come up in this ADDER, but it's worth thinking about in more complicated TMs.
+			break;
+		}
+
+		//Check if it has reached the halt state. As mentioned above, by convention this is state 2, but for convinience here it is 4.
+		if (state == 4)
+		{
+			//Program has succesfully terminated. The result is the contents of the tapehead (here input string) (a number written in unary, plus some blanks #)
+			return input;
+		}
+
+
+	}
+
+}
+
+//Encode a TM (program) into a string
+//This works by translating each row of instructions into a standard format (code word language, CWL), which can be concatonated and later decoded
+//See Cohen 1997:545
+
 
 
 int main()
 {
-
-
-	//Fix comparisons not working (not being able to use == on element from std::string) - use string.compare instead (returns 0 on true)
+	//Convert to prefix - outputs first pass
 	std::cout << ConvertToPrefixNotation("1+2+3+4") << std::endl;
-	//std::cout << ConvertToPrefixNotation("1+ 1") << std::endl;
-	//std::cout << ConvertToPrefixNotation("++1 * ") << std::endl;
+
+	//ADDER with a valid string
+	std::cout << ADDER(std::string("aaabaa")) << std::endl;
+
+	//ADDER with an invalid string
+	std::cout << ADDER(std::string("abaabaa")) << std::endl;
+
 
 
 }
